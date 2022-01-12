98db0e031f16fdf15a10bbdf7b8673c006ffa1c7
linhao <1334712251@qq.com>
2022-01-12 10:51:23 +0800

    提交修改

---
+++ src/main/java/com/example/demo/controller/Producer.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import com.example.demo.callback.MyCallBack;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequestMapping("/confirm")
+@Slf4j
+public class Producer {
+
+    public static final String CONFIRM_EXCHANGE_NAME = "confirm.exchange";
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private MyCallBack myCallBack;
+
+    //依赖注入 rabbitTemplate 之后再设置它的回调对象
+    //初始化
+    @PostConstruct
+    public void init() {
+        rabbitTemplate.setConfirmCallback(myCallBack);
+        rabbitTemplate.setReturnsCallback(myCallBack);
+    }
+
+    @GetMapping("/sendMessage/{message}")
+    public void sendMessage(@PathVariable String message) {
+        //指定消息 id 为 1
+        CorrelationData correlationData1 = new CorrelationData("1");
+
+        String routingKey = "key1";
+        rabbitTemplate.convertAndSend(CONFIRM_EXCHANGE_NAME, routingKey, message + routingKey, correlationData1);
+
+        CorrelationData correlationData2 = new CorrelationData("2");
+        routingKey = "key2";
+        rabbitTemplate.convertAndSend(CONFIRM_EXCHANGE_NAME, routingKey+"fff", message + routingKey, correlationData2);
+        log.info("发送消息内容:{}", message);
+
+    }
+}
